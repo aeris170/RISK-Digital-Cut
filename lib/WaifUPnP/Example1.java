8f22f9719d9aa91a195f3a99c963db8636bdb25b
Doğa Oruç <aeris170@users.noreply.github.com>
2019-03-04 21:27:10 +0300

    Push dependancy
    -MP-Risk now uses WaifUPnP!

---
+++ lib/WaifUPnP/Example1.java
@@ -0,0 +1,13 @@
+package WaifUPnP;
+
+import com.dosse.upnp.UPnP;
+
+/**
+ * @author Federico
+ */
+public class Example1 {
+	public static void main(String args[]) {
+		// this will attempt to open port TCP 4137. all errors will be ignored
+		UPnP.openPortTCP(4137);
+	}
+}
