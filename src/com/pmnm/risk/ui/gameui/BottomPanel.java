0a1d3bd59401669b0fbce4cd66b9588f33de9de3
Fazilet Simge ER <33057057+simge98@users.noreply.github.com>
2019-04-19 21:25:51 +0300

    Fix font size

--- src/com/pmnm/risk/ui/gameui/BottomPanel.java
+++ src/com/pmnm/risk/ui/gameui/BottomPanel.java
@@ -41,18 +41,19 @@ public class BottomPanel extends DoaPanel {
 		g.drawImage(DoaSprites.get("continentHolder"), Main.WINDOW_WIDTH * 0.430f, Main.WINDOW_HEIGHT * 0.950);
 		g.drawImage(DoaSprites.get("continentHolderIcon"), Main.WINDOW_WIDTH * 0.559f, Main.WINDOW_HEIGHT * 0.953f);
 
-		g.setFont(UIInit.UI_FONT.deriveFont(Font.PLAIN, 30f));
+		g.setFont(UIInit.UI_FONT.deriveFont(Font.PLAIN, 25f));
 		g.setColor(UIInit.FONT_COLOR);
 		g.drawString("" + GameManager.clickedHitArea.getProvince().getTroops(), Main.WINDOW_WIDTH * 0.454f + DoaSprites.get("garrisonHolder").getWidth() / 2f
-		        - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getTroops()) / 2f, Main.WINDOW_HEIGHT * 0.836f);
+		        - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getTroops()) / 2f, Main.WINDOW_HEIGHT * 0.858f);
+		g.setFont(UIInit.UI_FONT.deriveFont(Font.PLAIN, 15f));
 		g.setColor(GameManager.clickedHitArea.getProvince().getOwner().getColor());
 		g.drawString("" + GameManager.clickedHitArea.getProvince().getOwner().getName(), Main.WINDOW_WIDTH * 0.447f + DoaSprites.get("ownerHolder").getWidth() / 2f
-		        - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getOwner().getName()) / 2f, Main.WINDOW_HEIGHT * 0.836f + 60);
+		        - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getOwner().getName()) / 2f, Main.WINDOW_HEIGHT * 0.843f + 60);
 		g.setColor(UIInit.FONT_COLOR);
 		g.drawString("" + GameManager.clickedHitArea.getProvince().getName().toUpperCase(),
 		        Main.WINDOW_WIDTH * 0.436f + DoaSprites.get("provinceNameHolder").getWidth() / 2f
 		                - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getName().toUpperCase()) / 2f,
-		        Main.WINDOW_HEIGHT * 0.836f + 100);
+		        Main.WINDOW_HEIGHT * 0.845f + 100);
 		g.drawString("" + GameManager.clickedHitArea.getProvince().getContinent().getName().toUpperCase(),
 		        Main.WINDOW_WIDTH * 0.430f + DoaSprites.get("continentHolder").getWidth() / 2f
 		                - g.getFontMetrics().stringWidth("" + GameManager.clickedHitArea.getProvince().getContinent().getName().toUpperCase()) / 2f,
