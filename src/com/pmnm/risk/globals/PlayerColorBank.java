249f346062a1bbc44b5a006ea99a79c666a74951
Doğa Oruç <aeris170@gmail.com>
2022-07-07 22:40:47 +0300

    Introduce lombok

--- src/com/pmnm/risk/globals/PlayerColorBank.java
+++ src/com/pmnm/risk/globals/PlayerColorBank.java
@@ -2,9 +2,10 @@ package com.pmnm.risk.globals;
 
 import java.awt.Color;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public final class PlayerColorBank {
 
 	public static final Color[] colors = new Color[] { Color.RED, Color.BLUE, new Color(0x008080), new Color(0x800080), new Color(0xFFFF00), new Color(0xFF9200) };
-
-	private PlayerColorBank() {}
 }
