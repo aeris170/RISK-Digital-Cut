30de38c98684757c4280b8b02c45200a687aeb4c
Doğa Oruç <aeris170@gmail.com>
2022-07-17 01:41:58 +0300

    Implement an object to query system and runtime information

---
+++ src/com/pmnm/risk/main/SystemSpecs.java
@@ -0,0 +1,82 @@
+package com.pmnm.risk.main;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import doa.engine.core.DoaGraphicsFunctions;
+import doa.engine.scene.DoaObject;
+import doa.engine.scene.elements.renderers.DoaRenderer;
+import doa.engine.utils.hardwareinfo.*;
+
+@SuppressWarnings("serial")
+public final class SystemSpecs extends DoaObject {
+	
+	public static boolean isVisible = false;
+
+	private DoaRuntimeInfo runtime;
+	private DoaOSInfo os;
+	private DoaCPUInfo cpu;
+	private DoaMemoryInfo memory;
+	private DoaGPU gpu;
+	private DoaDisplay display;
+	
+	public SystemSpecs() {
+		runtime = DoaEngineInfo.getRuntimeInfo();
+		display = DoaEngineInfo.getDisplayInfo().getDisplayDevices().get(0);
+		gpu = DoaEngineInfo.getGpuInfo().getGraphicsCards().get(0);
+		memory = DoaEngineInfo.getMemoryInfo();
+		os = DoaEngineInfo.getOsInfo();
+		cpu = DoaEngineInfo.getCpuInfo();
+		setzOrder(Integer.MAX_VALUE);
+		addComponent(new Renderer());
+	}
+	
+	private class Renderer extends DoaRenderer {
+
+		private int fontSize = 24;
+		private Font font = new Font("Arial", Font.BOLD, fontSize);
+		private int outlineThickness = 1;
+		
+		@Override
+		public void render() {
+			if (!isVisible) { return; }
+			
+			DoaGraphicsFunctions.setFont(font);
+			DoaGraphicsFunctions.setColor(Color.green);
+			
+			renderText("DoaEngine Version: " + runtime.getVersion());
+			renderText("FPS: " + runtime.getFPS());
+			renderText("TICKS: " + runtime.getTPS());
+			renderText("----");
+			renderText("Operating System: " + os.getName());
+			renderText("----");
+			renderText("CPU: " + cpu.getModelName());
+			renderText("Clock Frequency: " + cpu.getMhz() + "MHz");
+			renderText("RAM: " + memory.getTotalMemory());
+			renderText("----");
+			renderText("GPU: " + gpu.getName());
+			renderText("C°: " + gpu.getTemperature());
+			renderText("RESOLUTION: " + display.getCurrentResolution());
+			renderText("REFRESH RATE: " + display.getRefreshRate());
+			
+			resetCounter();
+		}
+		
+		private int counter;
+		private void renderText(String text) {
+			int y = fontSize * ++counter;
+			
+			DoaGraphicsFunctions.setColor(Color.black);
+			for(int i = 1; i <= outlineThickness; i++) {
+				DoaGraphicsFunctions.drawString(text, -i, y);
+				DoaGraphicsFunctions.drawString(text, i, y);
+				DoaGraphicsFunctions.drawString(text, 0, y - i);
+				DoaGraphicsFunctions.drawString(text, 0, y + i);	
+			}
+			
+			DoaGraphicsFunctions.setColor(Color.green);
+			DoaGraphicsFunctions.drawString(text, 0, y);
+		}
+		private void resetCounter() { counter = 0; }
+	}
+}
