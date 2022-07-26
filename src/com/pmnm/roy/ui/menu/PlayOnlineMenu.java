cf8a27fdded87cdeb90026c0a0f92d5136c51ea1
Doğa Oruç <aeris170@gmail.com>
2022-07-26 06:37:55 +0300

    Put input popups to play online menu

--- src/com/pmnm/roy/ui/menu/PlayOnlineMenu.java
+++ src/com/pmnm/roy/ui/menu/PlayOnlineMenu.java
@@ -3,6 +3,8 @@ package com.pmnm.roy.ui.menu;
 import com.pmnm.roy.RoyButton;
 import com.pmnm.roy.RoyMenu;
 import com.pmnm.roy.ui.UIConstants;
+import com.pmnm.roy.ui.menu.extensions.InputPopup;
+import com.pmnm.roy.ui.menu.extensions.InputPopupDouble;
 
 import doa.engine.maths.DoaVector;
 
@@ -12,15 +14,15 @@ public class PlayOnlineMenu extends RoyMenu {
 	private static final DoaVector HOST_GAME_LOCATION 	= new DoaVector(1377f, 511f);
 	private static final DoaVector JOIN_GAME_LOCATION 	= new DoaVector(1377f, 584f);
 	private static final DoaVector BACK_LOCATION 		= new DoaVector(1377f, 803f);
+	
+	private InputPopup hostPopup;
+	private InputPopupDouble joinPopup;
 
 	public PlayOnlineMenu() {
 		RoyButton hostGameButton = RoyButton
 			.builder()
 			.textKey("HOST_GAME")
-			.action(() -> {
-				setVisible(false);
-				//UIConstants.getNewGameMenu().setVisible(true);
-			})
+			.action(() -> hostPopup.setVisible(true))
 			.build();
 		hostGameButton.setPosition(HOST_GAME_LOCATION);
 		addElement(hostGameButton);
@@ -28,10 +30,7 @@ public class PlayOnlineMenu extends RoyMenu {
 		RoyButton joinGameButton = RoyButton
 			.builder()
 			.textKey("JOIN_GAME")
-			.action(() -> {
-				setVisible(false);
-				//UIConstants.getLoadGameMenu().setVisible(true);
-			})
+			.action(() -> joinPopup.setVisible(true))
 			.build();
 		joinGameButton.setPosition(JOIN_GAME_LOCATION);
 		addElement(joinGameButton);
@@ -47,6 +46,19 @@ public class PlayOnlineMenu extends RoyMenu {
 		backButton.setPosition(BACK_LOCATION);
 		addElement(backButton);
 		
+		hostPopup = new InputPopup();
+		addElement(hostPopup);
+		
+		joinPopup = new InputPopupDouble();
+		addElement(joinPopup);
+		
 		setVisible(false);
+	}
+	
+	@Override
+	public void setVisible(boolean value) {
+		super.setVisible(value);
+		hostPopup.setVisible(false);
+		joinPopup.setVisible(false);
 	}
 }
