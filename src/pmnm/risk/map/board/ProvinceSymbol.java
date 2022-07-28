6b132af31ed186e34691d3d66983f3d36127aca4
Doğa Oruç <aeris170@gmail.com>
2022-07-28 09:35:02 +0300

    Render continent colored symbol if province is unoccupied

--- src/pmnm/risk/map/board/ProvinceSymbol.java
+++ src/pmnm/risk/map/board/ProvinceSymbol.java
@@ -13,6 +13,7 @@ import doa.engine.core.DoaGraphicsFunctions;
 import doa.engine.graphics.DoaSprites;
 import doa.engine.scene.DoaObject;
 import doa.engine.scene.elements.renderers.DoaRenderer;
+import pmnm.risk.game.IContinent;
 import pmnm.risk.game.IPlayer;
 import pmnm.risk.game.IProvince;
 
@@ -25,9 +26,31 @@ public final class ProvinceSymbol extends DoaObject {
 	}
 	
 	private ProvinceHitArea province;
+	private BufferedImage continentLogo;
+	private BufferedImage continentLogoUnoccupied;
 	
 	private ProvinceSymbol(ProvinceHitArea province) {
 		this.province = province;
+
+		IContinent provinceContinent = province.getProvince().getContinent();
+		continentLogo = DoaSprites.getSprite(provinceContinent.getAbbreviation());
+		continentLogoUnoccupied = new BufferedImage(continentLogo.getWidth(), continentLogo.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		
+		for (int yy = 0; yy < continentLogo.getHeight(); yy++) {
+			for (int xx = 0; xx < continentLogo.getWidth(); xx++) {
+				Color pixel = new Color(continentLogo.getRGB(xx, yy), true);
+				Color continentColor = provinceContinent.getColor();
+				Color newPixel = new Color(
+					continentColor.getRed(),
+					continentColor.getGreen(),
+					continentColor.getBlue(),
+					pixel.getAlpha()
+				);
+				
+				continentLogoUnoccupied.setRGB(xx, yy, newPixel.getRGB());
+			}
+		}
+		
 		setzOrder(ZOrders.PROVINCE_SYMBOL_Z);
 		addComponent(new Renderer());
 	}
@@ -39,30 +62,30 @@ public final class ProvinceSymbol extends DoaObject {
 		@Override
 		public void render() {
 			IProvince p = province.getProvince();
-			if (!p.isOccupied()) { return; }
-			IPlayer occupier = p.getOccupier();
 			float tx = province.getBounds().centerX;
 			float ty = province.getBounds().centerY;
 			
 			DoaGraphicsFunctions.setFont(UIConstants.getFont().deriveFont(Font.BOLD, 18f));
 			FontMetrics fm = DoaGraphicsFunctions.getFontMetrics();
-			DoaGraphicsFunctions.setColor(Color.BLACK);
-			BufferedImage ownerLogo = DoaSprites.getSprite("p" + occupier.getId() + "Pawn");
-			BufferedImage continentLogo = DoaSprites.getSprite(p.getContinent().getAbbreviation());
+			DoaGraphicsFunctions.setColor(new Color(1, 1, 1));
 			DoaGraphicsFunctions.drawImage(
-				continentLogo, 
+				p.isOccupied() ? continentLogo : continentLogoUnoccupied, 
 				tx - continentLogo.getWidth() * 0.33f, 
 				ty - continentLogo.getHeight() * 0.33f, 
 				continentLogo.getWidth() * 0.66f, 
 				continentLogo.getHeight() * 0.66f
 			);
+
+			if (!p.isOccupied()) { return; }
+			IPlayer occupier = p.getOccupier();
+			BufferedImage ownerLogo = DoaSprites.getSprite("p" + occupier.getId() + "Pawn");
 			DoaGraphicsFunctions.drawImage(
 				ownerLogo, 
 				tx - ownerLogo.getWidth() * 0.33f, 
 				ty - ownerLogo.getHeight() * 0.33f,
 				ownerLogo.getWidth() * 0.66f,
-		        ownerLogo.getHeight() * 0.66f
-	        );
+				ownerLogo.getHeight() * 0.66f
+			);
 			int troopCount = p.getNumberOfTroops();
 			String troops = "";
 			if (troopCount != Globals.UNKNOWN_TROOP_COUNT) {
