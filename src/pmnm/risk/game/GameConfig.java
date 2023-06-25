a2b725a33a097bfe8f730fe142e8b08be93af083
Doğa Oruç <aeris170@gmail.com>
2023-06-26 01:42:47 +0300

    Make GameConfig Serializable

--- src/pmnm/risk/game/GameConfig.java
+++ src/pmnm/risk/game/GameConfig.java
@@ -1,5 +1,7 @@
 package pmnm.risk.game;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Value;
@@ -8,7 +10,9 @@ import pmnm.risk.game.databasedimpl.Player;
 import pmnm.risk.map.MapConfig;
 
 @Value
-public class GameConfig {
+public class GameConfig implements Serializable {
+
+	private static final long serialVersionUID = 405673275700219342L;
 
 	@Getter @NonNull private final Player.Data[] data;
 	@Getter private final boolean isRandomPlacementEnabled;
